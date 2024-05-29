1837e11e753bef03e843b4370879a189195afa93
santa51107HD <santa51107@gmail.com>
2024-05-28 19:17:17 -0500

    v1.2.1

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -2,7 +2,24 @@ import { createRequire } from 'node:module'
 const require = createRequire(import.meta.url)
 
 export const readJSON = (path) => require(path)
-
+const util = require('util');
+export const exec = util.promisify(require("child_process").exec);
 // Función para convertir json a dzn
+export function jsontoDzn(data){
+  let dznContent= `ACTORES={${data.actores.join(', ')}};\n`;
+
+  dznContent+= `Escenas= [|`; 
+  data.participacion.forEach((escena, index) => {
+    dznContent += `  ${escena.join(', ')}`;
+    if (index < data.participacion.length - 1) {
+      dznContent += `, ${data.precioHora[index]}|\n`;
+    }
+  });
+  dznContent += `, ${data.precioHora[data.numeroActores-1]}|];\n`;
+
+  dznContent += `Duracion = [${data.duracionEscenas.join(', ')}];\n`;
+
+  return dznContent;
+}
 
 // Funcion para convertir resultado a json
